78a3eadacd1e8227378dd2ecde7d790682571d07
Kenichiro Nishioka <nishioka@3idea.jp>
2019-12-29 22:22:39 +0900

    Add authenticated role

--- main.tf
+++ main.tf
@@ -11,3 +11,62 @@ resource "aws_cognito_identity_pool" "kibana_identity_pool" {
   identity_pool_name               = "kibana_identity_pool"
   allow_unauthenticated_identities = true
 }
+
+resource "aws_iam_role" "kibana_cognito_authenticated" {
+  name = "kibana_cognito_authenticated"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "cognito-identity.amazonaws.com"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "cognito-identity.amazonaws.com:aud": "${aws_cognito_identity_pool.kibana_identity_pool.id}"
+        },
+        "ForAnyValue:StringLike": {
+          "cognito-identity.amazonaws.com:amr": "authenticated"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "kibana_identity_authenticated" {
+  name = "kibana_identity_authenticated_policy"
+  role = aws_iam_role.kibana_cognito_authenticated.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "mobileanalytics:PutEvents",
+        "cognito-sync:*",
+        "cognito-identity:*"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_cognito_identity_pool_roles_attachment" "main" {
+  identity_pool_id = aws_cognito_identity_pool.kibana_identity_pool.id
+
+  roles = {
+    "authenticated" = aws_iam_role.kibana_cognito_authenticated.arn
+  }
+}
